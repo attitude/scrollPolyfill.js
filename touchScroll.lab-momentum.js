e8a0eaf0d64a1a0fa46ba2b70f5237a50be726f2
Martin Adamko <martin@attitude.sk>
2014-07-21 20:04:08 +0200

    Rewrite for overflow

--- touchScroll.lab-momentum.js
+++ touchScroll.lab-momentum.js
@@ -1,23 +1,11 @@
-/*! touchScroll.js: Scroll Polyfil for Touch Devides | Copyright (c) 2014 Martin Adamko; Licensed MIT */
 window.addEventListener('load', function() {
     var // Scroll Top where touch starts
-        startPageY,
+        startClientY,
 
         // Momentum animation interval value
         momentumInterval,
         // Momentum acceleration
         momentumAcceleration,
-
-        // Scroll top when move started
-        scrollToStart
-            // Scroll top where to go to
-            = scrollToValue
-            // Scroll top where momentum scroll to go to
-            = momentumToValue
-            // Scroll top where previous move went to
-            = previousScrollToValue
-            // Default is current position
-            = document.body.scrollTop,
 
         // Artificial scroll event
         scrollToTriggered  = false,
@@ -38,16 +26,62 @@ window.addEventListener('load', function() {
         ticksSince     = 0,
 
         // Last move duration
-        ticksPerMove   = 0
+        ticksPerMove   = 0,
+
+        // Scroll targets
+        targetsY = [],
+        targetsYLength = 0
         ;
 
     document.body.addEventListener('touchstart', function(e) {
-        scrollToStart  = scrollToValue = momentumToValue = previousScrollToValue = document.body.scrollTop;
+        var target = e.target,
+            styles = getComputedStyle(target),
+            i
+            ;
+
         scrollToActive = true;
-        startPageY     = e.pageY;
+        targetsY       = styles.overflow != 'hidden' && styles.overflowY != 'hidden' ? [e.target] : [];
+
+        while(target.parentNode != null) {
+            target = target.parentNode;
+            if (target.nodeName == 'HTML') {
+                continue;
+            }
+
+            styles = getComputedStyle(target);
+
+            if (styles != null) {
+                if (styles.overflow != 'hidden' && styles.overflowY !== 'hidden') {
+                    targetsY.push(target);
+                }
+            }
+        }
+
+        targetsYLength = targetsY.length;
+
+        // Scroll Top where touch starts
+        startClientY = e.touches[0].clientY;
+
+        for (i=0; i<targetsYLength; i++) {
+            // Scroll top when move started
+            targetsY[i].scrollToStart =
+                // Scroll top where to go to
+                targetsY[i].scrollToValue =
+                // Scroll top where momentum scroll to go to
+                targetsY[i].momentumToValue =
+                // Scroll top where previous move went to
+                targetsY[i].scrollToValue =
+                // Default is current position
+                targetsY[i].scrollTop
+            ;
+        }
 
         // Start interval
         momentumInterval = setInterval(function() {
+            var i,
+                activeScrollIndex
+                ;
+
             ticksSince++;
 
             if(scrollToActive) {
@@ -65,50 +99,57 @@ window.addEventListener('load', function() {
                 return;
             }
 
-            momentumAcceleration = (scrollToValue - previousScrollToValue) / (ticksPerMove + 1);
+            activeScrollIndex = 0;
 
-            // Treshhold for slow momentums
-            if (Math.abs(momentumAcceleration) < 0.5 * (100 / ticksPerSecond)) {
-                // Self-deactivate
-                ticksSince = momentumTicks;
-                return;
-            }
+            for (i=0; i<targetsYLength;i++) {
+                if (activeScrollIndex != i) {
+                    continue;
+                }
 
-            // Quadratic easing out
-            momentumAcceleration -= momentumAcceleration * Math.pow((ticksSince/momentumTicks), coefDelta);
+                momentumAcceleration = (targetsY[i].scrollToValue - targetsY[i].previousScrollToValue) / (ticksPerMove + 1);
 
-            // Help with rounding; fixes juming feel
-            if (momentumAcceleration < 0) {
-                momentumAcceleration-= 0.5;
-            } else {
-                momentumAcceleration+= 0.5;
-            }
+                // Treshhold for slow momentums
+                if (Math.abs(momentumAcceleration) < 0.5 * (100 / ticksPerSecond)) {
+                    // Self-deactivate
+                    ticksSince = momentumTicks;
+                    return;
+                }
 
-            momentumToValue += momentumAcceleration;
+                // Quadratic easing out
+                momentumAcceleration -= momentumAcceleration * Math.pow((ticksSince/momentumTicks), coefDelta);
 
-            // Top edge boundary
-            if (momentumToValue < 0) {
-                // Self-deactivate
-                ticksSince = momentumTicks;
-                return;
-            }
+                // Help with rounding; fixes juming feel
+                if (momentumAcceleration < 0) {
+                    momentumAcceleration-= 0.5;
+                } else {
+                    momentumAcceleration+= 0.5;
+                }
 
-            // Skip same values for performance
-            if (document.body.scrollTop===parseInt(momentumToValue)) {
-                return;
-            }
+                targetsY[i].momentumToValue += momentumAcceleration;
 
-            // Not a native scroll
-            scrollToTriggered = true;
+                // Top edge boundary
+                if (targetsY[i].momentumToValue < 0) {
+                    activeScrollIndex++;
 
-            document.body.scrollTop = parseInt(momentumToValue);
-            // console.log(scrollToActive, 'momentum:', parseInt(momentumToValue));
+                    continue;
+                }
 
-            // Bottom edge boundary
-            if (document.body.scrollTop != parseInt(momentumToValue)) {
-                // Self-deactivate
-                ticksSince = momentumTicks;
-                return;
+                // Skip same values for performance
+                if (targetsY[i].scrollTop===parseInt(targetsY[i].momentumToValue)) {
+                    return;
+                }
+
+                // Not a native scroll
+                scrollToTriggered = true;
+
+                targetsY[i].scrollTop = parseInt(targetsY[i].momentumToValue);
+
+                // Bottom edge boundary
+                if (targetsY[i].scrollTop != parseInt(targetsY[i].momentumToValue)) {
+                    activeScrollIndex++;
+
+                    continue;
+                }
             }
         }, 1000 / ticksPerSecond);
     }, false);
@@ -125,39 +166,63 @@ window.addEventListener('load', function() {
 
     // Touch move triggers simulated scroll
     document.body.addEventListener('touchmove', function(e) {
+        var i,
+            activeScrollIndex
+            ;
+
         // Prevents default scroll action
         e.preventDefault();
-
-        // Remember from previous position to calculate momentum
-        previousScrollToValue = scrollToValue;
 
         // Remember ticks between last move to calculate momentum
         ticksPerMove = ticksSince;
 
-        // Calculate where we need to scroll to
-        scrollToValue = momentumToValue = startPageY - e.pageY + document.body.scrollTop;
-
-        // Top boundary
-        if (scrollToValue < 0) {
-            if (scrollToStart >= 0) {
-                scrollToStart += scrollToValue;
-                startPageY     = e.pageY;
-            }
-
-            if (scrollToStart < 0) { scrollToStart = 0; }
-
-            scrollToValue = previousScrollToValue = momentumToValue = startPageY - e.pageY + document.body.scrollTop;
-        }
-
         // Not a native scroll
         scrollToTriggered = true;
-        document.body.scrollTop = scrollToValue;
 
-        // Bottom boundary (failed to scroll)
-        if (scrollToValue > document.body.scrollTop) {
-            startPageY = e.pageY;
-            scrollToStart += document.body.scrollTop - scrollToValue;
-            scrollToValue = momentumToValue = document.body.scrollTop;
+        activeScrollIndex = 0;
+
+        for (i=0; i<targetsYLength;i++) {
+            if (activeScrollIndex != i) {
+                continue;
+            }
+
+            // Remember from previous position to calculate momentum
+            targetsY[i].previousScrollToValue = targetsY[i].scrollToValue;
+
+            // Calculate where we need to scroll to
+            targetsY[i].scrollToValue = targetsY[i].momentumToValue = startClientY - e.touches[0].clientY + targetsY[i].scrollToStart;
+
+            // Top boundary
+            if (targetsY[i].scrollToValue < 0) {
+                if (targetsY[i].nodeName==='BODY') {
+                    if (targetsY[i].scrollToStart >= 0) {
+                        targetsY[i].scrollToStart += targetsY[i].scrollToValue;
+                        startClientY     = e.touches[0].clientY;
+                    }
+
+                    if (targetsY[i].scrollToStart < 0) { targetsY[i].scrollToStart = 0; }
+
+                    // Calculate where we need to scroll to
+                    targetsY[i].previousScrollToValue = targetsY[i].scrollToValue = targetsY[i].momentumToValue = startClientY - e.touches[0].clientY + targetsY[i].scrollTop;
+                }
+
+                activeScrollIndex++; // Force to allow next target
+            }
+
+            // Apply scroll
+            targetsY[i].scrollTop = targetsY[i].scrollToValue;
+
+            // Bottom boundary (failed to scroll)
+            if (targetsY[i].scrollToValue > targetsY[i].scrollTop) {
+                if (targetsY[i].nodeName==='BODY') {
+                    startClientY-= targetsY[i].scrollToValue - targetsY[i].scrollTop;
+                    targetsY[i].scrollToValue = targetsY[i].momentumToValue = targetsY[i].scrollTop;
+                }
+
+                activeScrollIndex++; // Force to allow next target
+            }
+
+            // console.log('#'+i, 'Touch Y:', startClientY, '>', e.touches[0].clientY, 'scrollToStart:', targetsY[i].scrollToStart, 'scrollToValue:', targetsY[i].scrollToValue, '@', targetsY[i].scrollTop);
         }
 
         ticksSince = 0;
